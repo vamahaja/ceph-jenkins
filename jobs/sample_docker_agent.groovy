f909b582f3cc2cdd1c952a0ef4e819498f94a05e
Vaibhav Mahajan <vaibhavsm04@gmail.com>
2026-01-20 18:53:08 +0530

    enhancement: add docker cloud agent support (#6)
    Changes consists of -
      1. Docker cloud details to `casc.yaml`.
      2. Docker pipeline related plugins.
      3. Agent Containerfile.
      4. Sample pipeline to test Docker environment.
      5. Updated steps in `README.md`.
    
    Signed-off-by: Vaibhav Mahajan <vamahaja@redhat.com,vaibhav.sukhadev.mahajan@ibm.com>

---
+++ jobs/sample_docker_agent.groovy
@@ -0,0 +1,17 @@
+pipelineJob("sample-docker-pipeline") {
+    displayName('Test: Sample Docker Agent Pipeline')
+    description("A sample job to verify the Docker agent environment.")
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url("https://github.com/vamahaja/ceph-jenkins.git")
+                    }
+                    branches("main")
+                }
+            }
+            scriptPath("jobs/pipelines/sample_docker_agent/Jenkinsfile")
+    }
+}
