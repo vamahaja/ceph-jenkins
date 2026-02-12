eff8a8703936e7944e6af5fe1b6847a56f8bf801
Vaibhav Mahajan <vamahaja@redhat.com,vaibhav.sukhadev.mahajan@ibm.com>
2026-02-12 19:46:44 +0530

    fix: update logic to get ceph version
    Signed-off-by: Vaibhav Mahajan <vamahaja@redhat.com,vaibhav.sukhadev.mahajan@ibm.com>

--- jobs/sample_ceph_build_ci.groovy
+++ jobs/sample_ceph_build_ci.groovy
@@ -19,10 +19,7 @@ pipelineJob("sample-ceph-pipeline") {
         parameters {
             stringParam("CEPH_REPO", "https://github.com/ceph/ceph.git", "Ceph repository URL")
             stringParam("CEPH_BRANCH", "main", "Ceph branch to build")
-            stringParam("BASE_VERSION", "20.0.0", "Base version of Ceph to build")
             choiceParam("DISTRO", ["centos"], "Distribution to build for")
-            choiceParam("RELEASE", ["9"], "Release version")
-            choiceParam("DIST", ["el9"], "Distribution version")
             choiceParam("FLAVOR", ["default"], "Build flavor")
             stringParam("RPM_BUILD_OPTS", "--with tcmalloc --without selinux --without lto", "Additional RPM build options")
         }
